efc367db5611a2841f72d2ad138387442f027afa
Gibson Fahnestock <gib@uk.ibm.com>
2016-11-07 22:01:34 +0000

    Refactor and clean up code

--- t.rs
+++ t.rs
@@ -1,55 +1,50 @@
-#![feature(inclusive_range_syntax)]
 pub fn main() {
-    let results = test_all(3);
+    let results = test_all(4);
     println!("\nResults: {:?}", results );
 }
 
 const CH: [char; 40] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
-        '5', '6', '7', '8', '9', '.', ',', '-', '!'];
+        '5', '6', '7', '8', '9', '.', ',', '-', '!']; // Array of allowed chars
 
 
-pub fn test_all(maxlen: u32) -> Vec<String> {
-    let maxval = CH.len() - 1;
-    let mut output = Vec::new(); // Vec of okay outputs
-    let mut v = Vec::new();
-    for _ in 0..maxlen {
+pub fn test_all(length: usize) -> Vec<String> {
+    let mut output = Vec::new(); // Vec of okay passwords (Strings)
+    let mut v = Vec::new(); // Vector of passwords as numbers (corresponding to chars in CH)
+    let maxval = CH.len() - 1; // Maximum index in CH
+    for _ in 0..length {
         v.push(0); // create vector of lowest values
     }
-    println!("\n\n{:?}",v );
+    println!("\n Checking length {}, eg: {:?}", length, v); // Show Vector length (debug)
     'outer: loop {
         if v[0] != maxval {
-            v[0] += 1;
+            v[0] += 1; // Increment the first number
         } else {
             let mut i = 0;
             while v[i] == maxval {
-                if i == maxlen as usize - 1 { break 'outer; }
-                v[i] = 0;
-                i += 1;
+                if i == length - 1 { break 'outer; } // Checked all permutations so stop looping
+                v[i] = 0; // Number resets, maxval -> zero
+                i += 1;   // Carry the 1
             }
-            v[i] += 1;
+            v[i] += 1; // Increment first value that isn't maxval
         }
-        print!("{:?} ", v);
+        //print!("{:?} ", v); // Show each tried combination (debugging only)
         if let Some(s) = test(&v) { output.push(s); }
     }
-    if maxlen > 1 {
-        for i in test_all(maxlen-1){
-            output.push(i);
-        }
+    if length > 1 {
+        for i in test_all(length-1){ output.push(i); } // If password matches add to output
     }
-    return output;
+    return output; // Return all matching strings
 }
 
 pub fn test(v: &Vec<usize>) -> Option<String> {
-    let mut s = String::new();
-    for &i in v {
-        s.push(CH[i]);
-    }
+    let mut output = String::new();
+    for &i in v { output.push(CH[i]); } // Convert vector of indexes to string of chars (CH=hashmap)
+    let good_strings = vec!["as","p0","4"]; // Would be a hash result
     let valid: bool;
-    let good_strings = vec!["as","p0","4"];
     {
-        let s_ref: &str = &s;
-        valid = good_strings.contains(&s_ref);
+        let output_ref: &str = &output; // Need to finish this borrow before we return output
+        valid = good_strings.contains(&output_ref); // You'd check the hash here
     }
-    if valid { Some(s) } else { None }
+    if valid { Some(output) } else { None } // If the string is good, return it
 }
