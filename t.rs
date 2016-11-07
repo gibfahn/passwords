848bef511d52c201d4a09b8e66ac7f52e964b187
Gibson Fahnestock <gib@uk.ibm.com>
2016-11-07 21:16:04 +0000

    Working string version

--- t.rs
+++ t.rs
@@ -4,9 +4,13 @@ pub fn main() {
     println!("\nResults: {:?}", results );
 }
 
-pub fn test_all(maxlen: u32) -> Vec<Vec<u32>> {
-    // let chars = "abcdefghijklmnopqrstuvwxyz0123456789.,-!".chars();
-    let maxval = 12;
+const CH: [char; 40] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
+        '5', '6', '7', '8', '9', '.', ',', '-', '!'];
+
+
+pub fn test_all(maxlen: u32) -> Vec<String> {
+    let maxval = CH.len() - 1;
     let mut output = Vec::new(); // Vec of okay outputs
     let mut v = Vec::new();
     for _ in 0..maxlen {
@@ -26,7 +30,7 @@ pub fn test_all(maxlen: u32) -> Vec<Vec<u32>> {
             v[i] += 1;
         }
         print!("{:?} ", v);
-        if test(&v) { output.push(v.clone()) }
+        if let Some(s) = test(&v) { output.push(s); }
     }
     if maxlen > 1 {
         for i in test_all(maxlen-1){
@@ -36,7 +40,16 @@ pub fn test_all(maxlen: u32) -> Vec<Vec<u32>> {
     return output;
 }
 
-pub fn test(s: &Vec<u32>) -> bool {
-    let good_strings = vec![vec![2],vec![3,4,1]];
-    good_strings.contains(s)
+pub fn test(v: &Vec<usize>) -> Option<String> {
+    let mut s = String::new();
+    for &i in v {
+        s.push(CH[i]);
+    }
+    let valid: bool;
+    let good_strings = vec!["as","p0","4"];
+    {
+        let s_ref: &str = &s;
+        valid = good_strings.contains(&s_ref);
+    }
+    if valid { Some(s) } else { None }
 }
