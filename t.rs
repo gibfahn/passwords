ca64adf3d3d11a223eee2df8b75ade6ea56aab9d
Gibson Fahnestock <gib@uk.ibm.com>
2016-11-08 00:11:49 +0000

    Make it multi-threaded (not checked yet)

--- t.rs
+++ t.rs
@@ -1,4 +1,5 @@
 use std::ops::Deref;
+use rayon::par_iter::{IntoParallelIterator, ParallelIterator};
 pub fn main() {
     let results = test_all(4);
     println!("\nResults: {:?}", results );
@@ -10,7 +11,7 @@ const CH: [char; CH_LEN] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k
         '5', '6', '7', '8', '9', '.', ',', '-', '!']; // Array of allowed chars
 
 pub fn test_all(length: u32) -> Vec<String> {
-    (0..(CH.len().pow(length)+1)).filter(test).map(convert).collect::<Vec<String>>()
+    (0..(CH.len().pow(length)+1)).into_par_iter().filter(test).map(convert).collect::<Vec<String>>()
 }
 
 pub fn test(num: &usize) -> bool {
